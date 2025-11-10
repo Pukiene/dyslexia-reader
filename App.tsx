4919dddf3e5f3489f2d5975bfd47e81d6f7cbda5
Monika <m.rogoziniene@gmail.com>
2025-11-10 20:59:57 +0000

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import SpeechReader from './features/reader/SpeechReader';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <SpeechReader />
+        </div>
+    );
+}
+
+export default App;
+
+
